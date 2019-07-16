2c1b9721079a6bb956c0fb8a47e8f7d3e70db693
dreamingrainbow <michaeladennis@yahoo.com>
2019-07-15 20:31:03 -0700

    Update dreamingrainbow.js

--- src/components/dreamingrainbow.js
+++ src/components/dreamingrainbow.js
@@ -1,54 +1,50 @@
-import React, { Component } from 'react';
-
-class DreamingRainbow extends Component {
-  constructor(props){
-    super(props);
-    this.colorArray = ["Red","Orange","Yellow","Green","Blue","Purple"];
-    this.colorString = { "color" : this.colorArray[0] };
+import React from 'react';
+export default function DreamingRainbow(props) {
+  
+  var colorArray = ["Red","Orange","Yellow","Green","Blue","Purple"];
+  var colorString = { "color" : colorArray[0] };
+  function setColor(i) {
+    colorString = {"color" : colorArray[i]};
   }
 
-  setColor(i) {
-    this.colorString = {"color" : this.colorArray[i]};
-  }
-
-  splitByLetters(str) {
-    let arr = str.split('')
-    const mid = Math.floor(arr.length/2);
-    const leftArray = arr.slice(0,mid);
-    const rightArray = arr.slice(mid);
-    const leftSectionSize = Math.ceil(leftArray.length/3);
-    const rightSectionSize = Math.ceil(rightArray.length/3);
+  function splitByLetters(str) {
+    var arr = JSON.parse(JSON.stringify(str)).toString().split('');
+    
+    var mid = Math.floor(arr.length/2);
+    
+    var leftArray = arr.slice(0,mid);
+    var rightArray = arr.slice(mid);
+    
+    var leftSectionSize = Math.ceil(leftArray.length/3);
+    var rightSectionSize = Math.ceil(rightArray.length/3);
+    
     if (str.length <= 1) return {'0':str};
     
-    const sections = [];
-    const x = (arr, mid, SectionSize, sections) => {
-      const SplitSize = Math.floor(mid/SectionSize);
-      for(let i = 0;i <= SplitSize; i++){
-        if(i===0) {
-          sections.push(arr.slice(0, SectionSize).join('')) //dr
-        } else {
-          const t = arr.slice(SectionSize * i);
-          sections.push(t.slice(0, SectionSize).join(''));
-        }
-      }
-      return sections;
-    }
+    var sections = [];
+    var x = (arr, mid, SectionSize, sections) => {
+        var SplitSize = Math.floor(mid/SectionSize);
+        for(let i = 0;i <= SplitSize; i++){
+          if(i===0) {
+              sections.push(arr.slice(0, SectionSize).join('')); //dr
+           } else {
+              var t = arr.slice(SectionSize * i);
+               sections.push(t.slice(0, SectionSize).join(''));
+            };
+          }
+          return sections;
+    };
     x(leftArray, mid, leftSectionSize, sections);
-    x(rightArray, mid, rightSectionSize, sections);    
-    return sections;
-  }
-
-  componentWillMount() {
-    
-  }
-
-  render() {
-    //Initial Render Rainbow Text and Blackbackground
-    return(<div style={this.colorString}>{this.splitByLetters(this.props.phrase).map((section, i) => {
-      this.setColor(i);
-      return <span style={this.colorString} key={i}>{section}</span>
-    })}</div>)
-  }
+    x(rightArray, mid, rightSectionSize, sections);   
+  };
+  var DrEaMiNgRaInBoWD0TCoM = splitByLetters(props.phrase).concat((section, i) => {
+    setColor(i);
+    return (<span style={colorString}>{section}</span>);
+   });
+   
+   return(<main>{DrEaMiNgRaInBoWD0TCoM}</main>);
+  
 }
 
-export default DreamingRainbow;
+export DreamingRainbow as DreamingRainbows;
+export DreamingRainbow as Words2Rainbows;
+
